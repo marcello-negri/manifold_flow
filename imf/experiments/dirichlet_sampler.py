fixed numerical issues with transitionprob

imf/experiments/dirichlet_sampler.py
@@ -12,6 +12,8 @@ from abc import ABC, abstractmethod
 
 class Markov_dirichlet(ABC):
     def __init__(self, init_x, proposal_alphas, step_sizes):
+        self.accrate = 0.0
+        self.accrate_gamma = 0.98
         self.cur = init_x  # (b, n)
         self.device = init_x.device
         self.dtype = init_x.dtype
@@ -23,7 +25,8 @@ class Markov_dirichlet(ABC):
         self.step_sizes = step_sizes
         self.grid_alpha = grid_alpha
         self.grid_steps = grid_steps
-        self.average = torch.ones((self.dim,), device=self.device, dtype=self.dtype) / self.dim
+        self.minf = torch.tensor(float('-inf'), device=self.device, dtype=self.dtype)
+        #self.average = torch.ones((self.dim,), device=self.device, dtype=self.dtype) / self.dim
 
     def propose(self):
         proposed_grid = get_dirichlet_samples(self.alphas, self.chains, self.dim)
@@ -42,11 +45,11 @@ class Markov_dirichlet(ABC):
         fromx = self.eps_border(fromx)
         real_prop = fromx + (tox - fromx) / self.step_sizes[:,None,None]
         mask = torch.logical_and(torch.all(real_prop < 1.0, dim=-1), torch.all(real_prop > 0.0, dim=-1))
-        adjust_prop = torch.where(mask[...,None], real_prop, self.average)
-        logps_unnorm = get_logp_dirichlet(alpha=self.grid_alpha, x=adjust_prop)
-        logps = logps_unnorm - self.dim * torch.log(self.step_sizes[None,...,None])
-        exp = torch.where(mask, torch.exp(logps), 0.0)
-        return torch.log(exp.sum((0,1)))
+        adjust_prop = torch.where(mask[...,None], real_prop, fromx)
+        logps_unnorm = get_logp_dirichlet(alpha=self.grid_alpha, x=adjust_prop)[0]
+        logps = logps_unnorm - self.dim * torch.log(self.step_sizes[...,None])
+        masked_logps = torch.where(mask, logps, self.minf)
+        return torch.logsumexp(masked_logps, dim=0)
 
     @abstractmethod
     def log_p(self, x):
@@ -59,6 +62,7 @@ class Markov_dirichlet(ABC):
         t = self.transition(proposed, self.cur) - self.transition(self.cur, proposed)
         a = prop - self.curp - t
         mask = torch.logical_or(a > 0.0, torch.rand(a.shape, device=self.device, dtype=self.dtype) < torch.exp(a))
+        self.accrate = self.accrate_gamma * self.accrate + (1-self.accrate_gamma) * mask.to(dtype=torch.int32).sum() / mask.numel()
         self.cur = torch.where(mask[...,None], proposed, self.cur)
         self.curp = torch.where(mask, prop, self.curp)
 
@@ -125,12 +129,12 @@ device = "cuda"
 dtype = torch.float64
 burnin = 1000
 chains = 10000
-dim = 20  # TODO adjust
+dim = 200  # TODO adjust
 iterations = 5000
 subsample = 1000
-alphas_proposal = [0.2, 1.0]
+alphas_proposal = [0.2,1.0]
 # important to have 1.0 in the steps for successful sampling
-steps_proposal = [0.005,0.05,0.2,1.0]
+steps_proposal = [0.000001, 0.00001,1.0]
 init_alpha = torch.tensor([1.0], device=device, dtype=dtype)  # TODO adjust
 
 init = get_dirichlet_samples(init_alpha, chains, dim)[0]
@@ -146,7 +150,7 @@ startt = os.times()
 for i in range(burnin):
     next(it)
     if i % 500 == 0:
-        print("burnin", i)
+        print(f"burnin {i} with accrate {test.accrate}")
 
 # here all the samples are stored on the cpu
 samples = torch.zeros((iterations // subsample, chains, dim), dtype=dtype)
@@ -156,7 +160,7 @@ for i in range(iterations):
     else:
         next(it)
     if i % 500 == 0:
-        print("iteration ", i)
+        print(f"iteration {i} with accrate {test.accrate}")
 endt = os.times()
 dt = endt.user + endt.system - startt.user - startt.system
 print(f"elapsed time for {(burnin + iterations) * chains} total samples taken over {burnin + iterations} iterations in total:", dt)